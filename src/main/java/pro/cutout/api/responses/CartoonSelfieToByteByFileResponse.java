a5ab06d2a2a2a80a64e44819ff575d99fb1919c9
LP <422066139@qq.com>
2022-07-20 09:28:57 +0800

    test

--- src/main/java/pro/cutout/api/responses/CartoonSelfieToByteByFileResponse.java
+++ src/main/java/pro/cutout/api/responses/CartoonSelfieToByteByFileResponse.java
@@ -2,5 +2,5 @@ package pro.cutout.api.responses;
 
 import pro.cutout.api.CutoutResponse;
 
-public class CartoonSelfieToByteByFileResponse extends CutoutResponse {
+public class CartoonSelfieToByteByFileResponse extends CutoutResponse<byte[]> {
 }
